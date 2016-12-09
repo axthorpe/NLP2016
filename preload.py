9787ca2ccea6ca58205abf2ad78cf16015565435
Ubuntu <ubuntu@ip-172-31-6-146.us-west-2.compute.internal>
2016-12-09 20:58:54 +0000

    Pushing things

---
+++ preload.py
@@ -0,0 +1,10 @@
+import editdistance
+from nltk.corpus import wordnet as wn
+from nltk.wsd import lesk
+from nltk.corpus import cmudict
+import stress as st
+import random
+import music
+import wikipedia
+import copy
+import rhymer
